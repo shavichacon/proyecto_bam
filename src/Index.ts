82b66a5712ba6a81b2eb4c20df8ae0a36ef88f85
Javier Chacón <shavichacon@gmail.com>
2020-11-23 15:47:44 -0600

    Commit inicial de aplicación rest

---
+++ src/Index.ts
@@ -0,0 +1,11 @@
+class Mensaje {
+    imprimir(){
+        console.log("hola mundo 3")
+        console.log("hola mundo 4")
+        console.log("hola mundo 5")
+        console.log("hola mundo 6")
+    }
+}
+
+let m = new Mensaje();
+m.imprimir()
